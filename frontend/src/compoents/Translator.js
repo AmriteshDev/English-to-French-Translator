3c445b56202ac388766bbdf46aaac8815589ace8
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2024-02-23 17:06:07 +0530

    wip

--- frontend/src/compoents/Translator.js
+++ frontend/src/compoents/Translator.js
@@ -1,11 +1,14 @@
 import React, { Fragment, useState } from 'react'
 import '../App.css';
+import Loader from './Loader';
 
 const Translator = () => {
 
     const [englishtext, setEnglishText] = useState("");
+    const [loading, setLoading] = useState(false);
     const [translatedText, setTranslatedText] = useState("");
     const translator = async () => {
+        setLoading(true)
         try {
             const responce = await fetch("api/v1/translator", {
                 method: "POST",
@@ -17,6 +20,7 @@ const Translator = () => {
             const data = await responce.json();
             console.log(data.translation)
             setTranslatedText(data.translation)
+            setLoading(false)
 
         } catch (error) {
             console.log("Error:", error)
@@ -25,22 +29,27 @@ const Translator = () => {
 
     return (
         <Fragment>
-            <div className='container'>
-                <div>
-                    <div>
-                        <h2>Enter Your Text here(in English):</h2>
-                        <input type='text' value={englishtext} onChange={(e) => setEnglishText(e.target.value)} />
-                    </div>
-                    <button onClick={translator}>Translate</button>
-                    <div>
-                        <h2>Tranlated to French:</h2>
-                        <p>
-                            {translatedText}
-                        </p>
-                    </div>
-                </div>
+            {
+                loading ? <Loader /> :
+                    <Fragment>
+                        <div className='container'>
 
-            </div>
+                            <div>
+                                <h2>Enter Your Text here(in English):</h2>
+                                <input type='text' value={englishtext} onChange={(e) => setEnglishText(e.target.value)} />
+                            </div>
+                            <button onClick={translator}>Translate</button>
+                            <div>
+                                <h2>Tranlated to French:</h2>
+                                <p>
+                                    {translatedText}
+                                </p>
+                            </div>
+
+
+                        </div>
+                    </Fragment>
+            }
         </Fragment>
     )
 }
